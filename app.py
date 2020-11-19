404fc6968ddf4e7af82f6f94905301a7b24f9633
vaibhav gautam <vaibhav.gautam2308@gmail.com>
2020-11-19 15:54:56 +0530

    fixing path issue

--- app.py
+++ app.py
@@ -19,8 +19,10 @@ def receiveData():
         #captcha_response=request.form['g-recaptcha-response']
         # if(captcha_response==None or captcha_response==''):
         #     return 'Please enter captcha'
-        image_path=os.getcwd()+"\\\\"+uploaded_file.filename
-        model_path=os.getcwd()+"\\Models\\EN\\EN.h5"
+        image_path=os.getcwd()+"/"+uploaded_file.filename
+        model_path=os.getcwd()+"/Models/EN/EN.h5"
+        #image_path=os.getcwd()+"\\\\"+uploaded_file.filename
+        #model_path=os.getcwd()+"\\Models\\EN\\EN.h5"
         print(image_path,'\n',model_path)
         return getClass.load_image(image_path,model_path)
 
